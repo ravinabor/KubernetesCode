2129b5151494c667e909ebaf664ba815ff94c2ea
ravinabor <121058589+ravinabor@users.noreply.github.com>
2023-01-15 21:30:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Deployed using GitOps version1.4-ravina borkar is my name'
+    return 'Deployed using GitOps version1.4- must resflect '
